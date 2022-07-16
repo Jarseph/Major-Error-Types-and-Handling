96b4bcd27399d4ad104a9ef1242c4eaef31c7986
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-07-16 15:28:52 -0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const notes = require(path.resolve("src/data/notes-data"));
+
+app.use(express.json());
+
+app.get("/notes/:noteId", (req, res) => {
+  const noteId = Number(req.params.noteId);
+  const foundNote = notes.find((note) => note.id === noteId);
+  if (foundNote) {
+    res.json({ data: foundNote });
+  } else {
+ next({
+      status: 400,
+      message: "A text property is required"
+    })
+}
+});
+
+app.get("/notes", (req, res) => {
+  res.json({ data: notes });
+});
+
+let lastNoteId = notes.reduce((maxId, note) => Math.max(maxId, note.id), 0);
+
+app.post("/notes", (req, res) => {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    const newNote = {
+      id: ++lastNoteId, // Increment last id then assign as the current ID
+      text,
+    };
+    notes.push(newNote);
+    res.status(201).json({ data: newNote });
+}
+});
+
+// Not found handler
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message:`Not found: ${req.originalUrl}` 
+  });
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
+});
+
+module.exports = app;
